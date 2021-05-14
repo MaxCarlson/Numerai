6a5c1b4d9a26783ac383c377feda519deeccd60a
Maxwell Carlson <bart4282819@gmail.com>
2021-05-13 17:07:54 -0700

    Added batchnorm, model saving, leaky relu, etc

--- Numerai/Numerai.py
+++ Numerai/Numerai.py
@@ -36,14 +36,14 @@ NAPI.download_current_dataset(dest_path=DIR, unzip=True)
 
 def printCorrelation(df):
     corr_matrix = df.corr()
-    corr_matrix = corr_matrix['target'].sort_values(ascending=False, method='spearman')
+    corr_matrix = corr_matrix['target'].sort_values(ascending=False)
     print(corr_matrix)
     return corr_matrix
 
 class AutoEncoder():
     
     lr = 0.009
-    epochs = 3
+    epochs = 100
     batchSize = 128
     stopping = keras.callbacks.EarlyStopping(
         monitor="val_loss",
@@ -58,6 +58,8 @@ class AutoEncoder():
     featureThreshold = 0.01
 
     def __init__(self):
+        self.encoder = None
+
         # Note: Current best:
         # 310, 256, 196, 128: lr=0.009, decay_steps=6800, decay_rate=0.95
         # act=tanh, out=sigmoid, loss=binary_entropy, optimizer=adam
@@ -65,53 +67,54 @@ class AutoEncoder():
         # Features > 0.01: 0.0148, 0.0131
 
         self.model = models.Sequential()
-        self.model.add(layers.Dense(310, activation='tanh'))
-        self.model.add(layers.Dense(256, activation='tanh'))
-        self.model.add(layers.Dense(196, activation='tanh'))
-        #self.model.add(layers.Dense(128, activation='tanh'))
-        #self.model.add(layers.Dense(64, activation='tanh'))
-        self.model.add(layers.Dense(128, activation='tanh', name='OutputLayer'))
-        #self.model.add(layers.Dense(64, activation='tanh'))
-        #self.model.add(layers.Dense(128, activation='tanh'))
-        self.model.add(layers.Dense(196, activation='tanh'))
-        self.model.add(layers.Dense(256, activation='tanh'))
+        self.model.add(layers.Dense(310))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dense(256))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dense(196))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.Dense(128))
+        #self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.Dense(64))
+        self.model.add(layers.Dense(128, name='OutputLayer'))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization()) ?
+        #self.model.add(layers.Dense(128))
+        #self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dense(196))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dense(256))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
         self.model.add(layers.Dense(310, activation='sigmoid'))
         self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.decay), 
               #loss=keras.losses.mean_squared_error,
               loss=keras.losses.binary_crossentropy,
               metrics=[])
 
-    def fit(self, data):
-        history = self.model.fit(x=data[:,:-1], y=data[:,:-1], epochs=self.epochs, 
-                                 batch_size=self.batchSize, validation_split=0.1, shuffle=True, 
-                                 callbacks=[self.stopping])
-        
-        # Testing 
-        #history = self.model.fit(x=data[:,:-1], y=data[:,:-1], epochs=1, 
-        #                         batch_size=self.batchSize, shuffle=True, 
-        #                         steps_per_epoch=len(data)//32//self.batchSize)
+    def fit(self, data, val):
+        if False:
+            self.model = keras.models.load_model('./aeModels/autoencoder-0.423')
 
-        # Build a model to produce the compressed output from the autoencoder
+        else:
+            hist = self.model.fit(x=data[:,:-1], y=data[:,:-1], epochs=self.epochs, 
+                                     batch_size=self.batchSize, steps_per_epoch=100,
+                                     validation_data=val,
+                                     shuffle=True, callbacks=[self.stopping])
+
+            self.model.save('./aeModels/autoencoder-{}'.format(round(hist.history['val_loss'][-1], 3)))
         aeOutput = self.model.get_layer(name='OutputLayer').output
-        aeModel  = keras.Model(inputs=self.model.input, outputs=aeOutput)
-        p = aeModel.predict(x=data[:,:-1])
+        self.encoder = keras.Model(inputs=self.model.input, outputs=aeOutput)
 
+    def predict(self, data):
+        p = self.encoder.predict(x=data[:,:-1])
         return p
-
-    def findTopOutputs(self, corr_matrix):
-        idxs = []
-        for i in range(1, len(corr_matrix)):
-            if corr_matrix.iloc[i, 1] < self.featureThreshold:
-                break
-            idxs.append(corr_matrix.iloc[i, 0])
-            
-        return i, idxs
-
-    def save(self, corr_matrix):
-        greaterThanBase, idxs = self.findTopOutputs(corr_matrix)
-        self.model.save(filepath=('./aeModels/autoencoder-{}-%s.h5'.format(greaterThanBase, ) % ','.join(idxs))
-                        , overwrite=False, include_optimizer=False)
-
 
 
 # Read the csv file into a pandas Dataframe as float16 to save space
@@ -139,7 +142,8 @@ def main():
     # The training data is used to train your model how to predict the targets.
     training_data, train = read_csv("data/numerai_dataset_263/numerai_training_data.csv")
     # The tournament data is the data that Numerai uses to evaluate your model.
-    #tournament_data = read_csv("data/numerai_dataset_263/numerai_tournament_data.csv")
+    tournament_data = read_csv("data/numerai_dataset_263/numerai_tournament_data.csv")[0]
+    validation_data = tournament_data[tournament_data.data_type == "validation"]
     #printCorrelation(training_data)
 
     feature_names = [
@@ -147,11 +151,11 @@ def main():
     ]
     print(f"Loaded {len(feature_names)} features")
 
-    aeout = ae.fit(train)
+    ae.fit(train, (validation_data[feature_names], validation_data['target']))
+    aeout = ae.predict(train)
     df = pd.DataFrame(aeout)
     df['target'] = train[:,-1:]
     corr_matrix = printCorrelation(df)
-    ae.save(corr_matrix)
     
 
 
