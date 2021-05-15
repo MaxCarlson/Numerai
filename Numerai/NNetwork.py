68a90f2e473058ae1938f638a69d438822b142c6
Maxwell Carlson <bart4282819@gmail.com>
2021-05-14 20:03:14 -0700

    Moved data loading to own function. Added nn module.

---
+++ Numerai/NNetwork.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import keras as K
+from keras import layers
+from keras import models
+
+# TF differentiable correaltion function(s)?
+def correlation_coefficient_loss(y_true, y_pred):
+    x = y_true
+    y = y_pred
+    mx = K.mean(x)
+    my = K.mean(y)
+    xm, ym = x-mx, y-my
+    r_num = K.sum(tf.multiply(xm,ym))
+    r_den = K.sqrt(tf.multiply(K.sum(K.square(xm)), K.sum(K.square(ym))))
+    r = r_num / r_den
+
+    r = K.maximum(K.minimum(r, 1.0), -1.0)
+    return 1 - K.square(r)
+
+def correlation_tf(x, y):    
+    mx = tf.math.reduce_mean(x)
+    my = tf.math.reduce_mean(y)
+    xm, ym = x-mx, y-my
+    r_num = tf.math.reduce_mean(tf.multiply(xm,ym))        
+    r_den = tf.math.reduce_std(xm) * tf.math.reduce_std(ym)
+    return r_num / r_den
+
+
+class NNModel():
+    lr = 0.001
+    epochs = 100
+    batchSize = 128
+
+    stopping = keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        patience=3,
+        mode="auto",
+        baseline=None,
+        min_delta=0.0001,
+        restore_best_weights=True)
+
+    def __init__(self):
+        self.model = models.Sequential()
+        self.model.add(layers.Dense(310))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(256))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(196))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(128))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(64))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(32))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(16))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(8))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(4))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(2))
+        self.model.add(layers.LeakyReLU(alpha=0.3))
+        self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.Dropout(0.15))
+        self.model.add(layers.Dense(1))
+        self.model.add(layers.Activation(K.activations.sigmoid))
+
+        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.lr), 
+              #loss=keras.losses.mean_squared_error,
+              loss=keras.losses.binary_crossentropy,
+              metrics=[])
+
+
+        def fit(self, features, val):
+            hist = self.model.fit(x=features.values, y=features.values, epochs=self.epochs, 
+                            batch_size=self.batchSize, #steps_per_epoch=10,
+                            validation_data=(val.values, val.values),
+                            shuffle=True, callbacks=[self.stopping])
+
+            self.model.save('./nnModels/nn-{}'.format(round(hist.history['val_loss'][-1], 3)))
+
+        def predict(self, features):
+            p = self.model.predict(x=features.values)
+            return p
--- Numerai/Numerai.py
+++ Numerai/Numerai.py
@@ -1,5 +1,6 @@
 # Ideas
 # Maybe add difference in eras as feature?
+# Add cyclical learning rates
 
 # Feature Engineering
 # # # # # # # # # # # # 
@@ -20,6 +21,8 @@ from keras import layers
 from keras import models
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+
+from NNetwork import NNModel
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -55,18 +58,20 @@ def payout(scores):
 
 class AutoEncoder():
     
-    lr = 0.009
+    lr = 0.008
     epochs = 100
     batchSize = 128
+
     stopping = keras.callbacks.EarlyStopping(
         monitor="val_loss",
         patience=3,
         mode="auto",
         baseline=None,
+        min_delta=0.0001,
         restore_best_weights=True)
 
     decay = keras.optimizers.schedules.ExponentialDecay(
-        lr, decay_steps=3400*2, decay_rate=0.95)
+        lr, decay_steps=3400, decay_rate=0.95)
 
     featureThreshold = 0.01
 
@@ -74,10 +79,6 @@ class AutoEncoder():
         self.encoder = None
 
         # Note: Current best:
-        # 310, 256, 196, 128: lr=0.009, decay_steps=6800, decay_rate=0.95
-        # act=tanh, out=sigmoid, loss=binary_entropy, optimizer=adam
-        # Epochs=3, val_loss=0.606
-        # Features > 0.01: 0.0148, 0.0131
 
         self.model = models.Sequential()
         self.model.add(layers.Dense(310))
@@ -89,17 +90,17 @@ class AutoEncoder():
         self.model.add(layers.Dense(196))
         self.model.add(layers.LeakyReLU(alpha=0.3))
         self.model.add(layers.BatchNormalization())
-        self.model.add(layers.Dense(128))
-        self.model.add(layers.LeakyReLU(alpha=0.3))
-        self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.Dense(128))
+        #self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
 
         self.model.add(layers.Dense(128, name='OutputLayer'))
         self.model.add(layers.LeakyReLU(alpha=0.3))
         self.model.add(layers.BatchNormalization())
         
-        self.model.add(layers.Dense(128))
-        self.model.add(layers.LeakyReLU(alpha=0.3))
-        self.model.add(layers.BatchNormalization())
+        #self.model.add(layers.Dense(128))
+        #self.model.add(layers.LeakyReLU(alpha=0.3))
+        #self.model.add(layers.BatchNormalization())
         self.model.add(layers.Dense(196))
         self.model.add(layers.LeakyReLU(alpha=0.3))
         self.model.add(layers.BatchNormalization())
@@ -107,7 +108,7 @@ class AutoEncoder():
         self.model.add(layers.LeakyReLU(alpha=0.3))
         self.model.add(layers.BatchNormalization())
         self.model.add(layers.Dense(310, activation='sigmoid'))
-        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.decay), 
+        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.lr), 
               #loss=keras.losses.mean_squared_error,
               loss=keras.losses.binary_crossentropy,
               metrics=[])
@@ -149,9 +150,7 @@ def read_csv(file_path):
     df = pd.read_csv(file_path, dtype=dtypes, index_col=0)
     return df
 
-
-def main():
-    ae = AutoEncoder()
+def loadData():
     print("Loading data...")
     # The training data is used to train your model how to predict the targets.
     training_data = read_csv("data/numerai_dataset_263/numerai_training_data.csv")
@@ -164,6 +163,11 @@ def main():
         f for f in training_data.columns if f.startswith("feature")
     ]
     print(f"Loaded {len(feature_names)} features")
+
+    return training_data, tournament_data, validation_data, feature_names
+
+def runAE(training_data, tournament_data, validation_data, feature_names):
+    ae = AutoEncoder()
 
     #print('Printing features: \n', validation_data[feature_names])
     #print('Printing targets: \n', validation_data['target'])
@@ -179,29 +183,34 @@ def main():
 
     return
 
+def runNN(training_data, tournament_data, validation_data, feature_names):
+    model = NNModel()
 
-    #training_data[PREDICTION_NAME] = model.predict(training_data[feature_names], training_data[TARGET_NAME])
-    #tournament_data[PREDICTION_NAME] = model.predict(tournament_data[feature_names], tournament_data[TARGET_NAME])
+    training_data[PREDICTION_NAME] = model.predict(training_data[feature_names], training_data[TARGET_NAME])
+    tournament_data[PREDICTION_NAME] = model.predict(tournament_data[feature_names], tournament_data[TARGET_NAME])
 
-    ## Check the per-era correlations on the training set (in sample)
-    #train_correlations = training_data.groupby("era").apply(score)
-    #print(f"On training the correlation has mean {train_correlations.mean()} and std {train_correlations.std(ddof=0)}")
-    #print(f"On training the average per-era payout is {payout(train_correlations).mean()}")
-    #
-    #"""Validation Metrics"""
-    ## Check the per-era correlations on the validation set (out of sample)
-    #validation_data[PREDICTION_NAME] = tournament_data[PREDICTION_NAME]
-    #validation_correlations = validation_data.groupby("era").apply(score)
-    #print(f"On validation the correlation has mean {validation_correlations.mean()} and "
-    #      f"std {validation_correlations.std(ddof=0)}")
-    #print(f"On validation the average per-era payout is {payout(validation_correlations).mean()}")
-    #
-    ## Check the "sharpe" ratio on the validation set
-    #validation_sharpe = validation_correlations.mean() / validation_correlations.std(ddof=0)
-    #print(f"Validation Sharpe: {validation_sharpe}")
+    # Check the per-era correlations on the training set (in sample)
+    train_correlations = training_data.groupby("era").apply(score)
+    print(f"On training the correlation has mean {train_correlations.mean()} and std {train_correlations.std(ddof=0)}")
+    print(f"On training the average per-era payout is {payout(train_correlations).mean()}")
+    
+    """Validation Metrics"""
+    # Check the per-era correlations on the validation set (out of sample)
+    validation_data[PREDICTION_NAME] = tournament_data[PREDICTION_NAME]
+    validation_correlations = validation_data.groupby("era").apply(score)
+    print(f"On validation the correlation has mean {validation_correlations.mean()} and "
+          f"std {validation_correlations.std(ddof=0)}")
+    print(f"On validation the average per-era payout is {payout(validation_correlations).mean()}")
+    
+    # Check the "sharpe" ratio on the validation set
+    validation_sharpe = validation_correlations.mean() / validation_correlations.std(ddof=0)
+    print(f"Validation Sharpe: {validation_sharpe}")
     
 
 
 
 if __name__ == "__main__":
-    main()
+    training_data, tournament_data, validation_data, feature_names = loadData()
+
+    #runAE()
+    runNN(training_data, tournament_data, validation_data, feature_names)
